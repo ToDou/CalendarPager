7e1ccc043a92e46d35bd1937c4c6340f2bf076d9
ToDou <yytodou123@gmail.com>
2015-04-30 21:26:53 +0800

    fix scroll

--- app/src/main/java/com/tudou/calendarpager/ui/view/HeaderViewPager.java
+++ app/src/main/java/com/tudou/calendarpager/ui/view/HeaderViewPager.java
@@ -3,11 +3,13 @@ package com.tudou.calendarpager.ui.view;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
 
 /**
  * Created by tudou on 15-4-30.
  */
 public class HeaderViewPager extends ViewPager {
+  private final static String TAG = "HeaderViewPager";
 
   public HeaderViewPager(Context context) {
     this(context, null);
@@ -19,8 +21,10 @@ public class HeaderViewPager extends ViewPager {
 
   public void onContentPageScrolled(int position, float positionOffset,
       int positionOffsetPixels) {
+    Log.e(TAG, "position: " + position + "      positionOffset: " + positionOffset + "     positionOffsetPicxels: " + positionOffsetPixels);
     if (position % 6 == 0) {
-      this.onPageScrolled(position / 6, positionOffset, positionOffsetPixels);
+      //this.onPageScrolled(position / 6, positionOffset, positionOffsetPixels);
+
     }
   }
 
