71c3448ddd555f8aab0b7916b2ab2e9d1e020633
ToDou <yytodou123@gmail.com>
2015-05-01 23:11:51 +0800

    add the gif

--- app/src/main/java/com/tudou/calendarpager/ui/fragment/SimpleFragment.java
+++ app/src/main/java/com/tudou/calendarpager/ui/fragment/SimpleFragment.java
@@ -7,12 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.tudou.calendarpager.R;
 import com.tudou.calendarpager.model.CalendarDay;
+import com.tudou.calendarpager.util.DayUtils;
+import java.awt.font.TextAttribute;
 
 /**
  * Created by tudou on 15-4-30.
  */
 public class SimpleFragment extends Fragment {
+
+  @InjectView(R.id.text) TextView mText;
 
   private CalendarDay mCalendarDay;
 
@@ -24,8 +31,12 @@ public class SimpleFragment extends Fragment {
 
   @Override public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    TextView textView = new TextView(getActivity());
-    textView.setText(mCalendarDay.year + " - " + mCalendarDay.getMonth() + " - " + mCalendarDay.day);
-    return textView;
+    return inflater.inflate(R.layout.fragment_simple, container, false);
+  }
+
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    ButterKnife.inject(this, view);
+    mText.setText("This is at: " + DayUtils.formatEnglishTime(mCalendarDay.getTime()));
   }
 }
