2f9b32dc7d26aedcb06bd2797dfd2e2f5d207f0b
ToDou <yangyu@loopeer.com>
2015-07-08 17:41:30 +0800

    fix the calendar example view

--- app/src/main/java/com/tudou/calendarpager/ui/activity/ExpandCalendarActivity.java
+++ app/src/main/java/com/tudou/calendarpager/ui/activity/ExpandCalendarActivity.java
@@ -2,38 +2,45 @@ package com.tudou.calendarpager.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 import android.widget.Toast;
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-import com.test.tudou.library.expandcalendar.view.ExpandCalendarView;
+
 import com.test.tudou.library.expandcalendar.view.ExpandCalendarMonthView;
+import com.test.tudou.library.expandcalendar.view.ExpandCalendarView;
 import com.test.tudou.library.model.CalendarDay;
 import com.tudou.calendarpager.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by tudou on 15-5-19.
  */
 public class ExpandCalendarActivity extends ActionBarActivity implements ExpandCalendarMonthView.OnDayClickListener {
 
-  @InjectView(R.id.view_calendar) ExpandCalendarView mMonthPagerView;
+    @InjectView(R.id.view_calendar)
+    ExpandCalendarView mMonthPagerView;
+    @InjectView(R.id.text_example)
+    TextView textExample;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_expand_calendar);
-    ButterKnife.inject(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expand_calendar);
+        ButterKnife.inject(this);
 
-    updateData();
-  }
+        updateData();
+    }
 
-  private void updateData() {
-    mMonthPagerView.setData(new CalendarDay(2015, 5, 4), new CalendarDay(2020, 12, 2));
-    mMonthPagerView.setOnDayClickListener(this);
-    mMonthPagerView.setSelectDay(new CalendarDay(2016, 10, 1));
+    private void updateData() {
+        mMonthPagerView.setData(new CalendarDay(2015, 5, 4), new CalendarDay(2020, 12, 2));
+        mMonthPagerView.setOnDayClickListener(this);
+        mMonthPagerView.setSelectDay(new CalendarDay(2016, 10, 16));
 
-  }
+    }
 
-  @Override public void onDayClick(CalendarDay calendarDay) {
-    Toast.makeText(this, calendarDay.getDayString(), Toast.LENGTH_SHORT).show();
-  }
+    @Override
+    public void onDayClick(CalendarDay calendarDay) {
+        textExample.setText("Click at " + calendarDay.getDayString());
+    }
 }
